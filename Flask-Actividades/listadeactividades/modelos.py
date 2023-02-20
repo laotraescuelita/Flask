dc62d780a39cafee48c2b21189f320cab92d89fd
Erick Fco Acosta Mtz <92598716+laotraescuelita@users.noreply.github.com>
2023-02-20 12:27:32 -0600

    Mejorar el app

--- Flask-Actividades/listadeactividades/modelos.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from listadeactividades import db
-
-class MiListaDeActividades(db.Model):
-	id = db.Column( db.Integer, primary_key=True )
-	titulo = db.Column( db.String( 100 ))
-	completado = db.Column(db.Boolean)
--- Flask-Actividades/run.py
+++
@@ -1,5 +0,0 @@
-from listadeactividades import app
-
-if __name__ == '__main__':
-    #db.create_all()
-    app.run(debug=True)
